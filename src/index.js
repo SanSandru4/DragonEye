f87bfe82af8f94605ac32bc64f9e2d4a3d578ebf
Oleksandr Prylutskiy <fleks1986@gmail.com>
2022-08-07 23:05:34 +0300

    refactoring file state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import reportWebVitals from './reportWebVitals';
-import { rerenderingEntireTree } from './render'
-import { state } from './redux/state'
+import { store } from './redux/state'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-rerenderingEntireTree(state);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let rerenderingEntireTree = (state) => {
+  root.render(
+    <BrowserRouter>
+      <React.StrictMode>
+        <App state = { state } addPost = { store.addPost.bind(store) } updateNewPost = { store.updateNewPostText.bind(store) } />
+      </React.StrictMode>
+    </BrowserRouter>
+  );  
+}
+
+
+rerenderingEntireTree(store.getState());
+
+store.subscribe(rerenderingEntireTree);
 
 reportWebVitals();
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,47 +1,53 @@
-import { rerenderingEntireTree } from './../render';
-
-export let state = {
-    dialogPage: {
-      dialogs: [
-        { id: 1, name: 'Dimon' },
-        { id: 2, name: 'Max' },
-        { id: 3, name: 'Vlad' },
-        { id: 4, name: 'Vadim' },
-        { id: 5, name: 'Sasha' },
-        { id: 6, name: 'Vitalya' }
-      ],
-      messages: [
-        { id: 1, message: 'Hi' },
-        { id: 2, message: 'How are your?' },
-        { id: 3, message: 'Yo' },
-        { id: 4, message: 'Yo' },
-        { id: 5, message: 'Yo' }
-      ]
-    },
-    blogPage: {
-        posts: [
-            {id: 1, message: 'How are you?', likesCount: 12},
-            {id: 2, message: "It's my first post", likesCount: 9}
+export let store = {
+    _state: {
+        dialogPage: {
+        dialogs: [
+            { id: 1, name: 'Dimon' },
+            { id: 2, name: 'Max' },
+            { id: 3, name: 'Vlad' },
+            { id: 4, name: 'Vadim' },
+            { id: 5, name: 'Sasha' },
+            { id: 6, name: 'Vitalya' }
         ],
-        newPostText: 'new text message!'
-    }
+        messages: [
+            { id: 1, message: 'Hi' },
+            { id: 2, message: 'How are your?' },
+            { id: 3, message: 'Yo' },
+            { id: 4, message: 'Yo' },
+            { id: 5, message: 'Yo' }
+        ]
+        },
+        blogPage: {
+            posts: [
+                {id: 1, message: 'How are you?', likesCount: 12},
+                {id: 2, message: "It's my first post", likesCount: 9}
+            ],
+            newPostText: 'new text message!'
+        }
+    },
+    getState() {
+        return this._state;
+    },
+    _callSubscriber() {
+        console.log('State it was change');
+    },
+    addPost() {
+        let newPost = {
+            id: 3,
+            message: this._state.blogPage.newPostText,
+            likesCount: 0
+        };
+        this._state.blogPage.posts.push(newPost);
+        this._state.blogPage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.blogPage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    }, 
 }
-
-export let addPost = () => {
-    let newPost = {
-        id: 3,
-        message: state.blogPage.newPostText,
-        likesCount: 0
-    };
-
-    state.blogPage.posts.push(newPost);
-    state.blogPage.newPostText = '';
-    rerenderingEntireTree(state);
-};
-
-export let updateNewPostText = (newText) => {
-  state.blogPage.newPostText = newText;
-  rerenderingEntireTree(state);
-}
-
-
+ 
+// export default store;
